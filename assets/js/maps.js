aa8c5a4c501dbd3128bcdd17dbd548a2bb16ebe9
Oumnia El Mokretar <oumnia.elmokretar@railconcept.fr>
2024-01-09 17:45:20 +0100

    fixed Gmaps

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,97 +1,80 @@
-// this bypass some origin checks with the api key
-(() => {
-    "use strict";
+'use strict';
 
-    const appendChild = Element.prototype.appendChild;
+async function initMaps() {
+    const { Map } = await google.maps.importLibrary("maps");
 
-    const urlCatchers = ["/AuthenticationService.Authenticate?", "/QuotaService.RecordEvent?"];
+    const gmapsDivs = document.getElementsByClassName("gmap");
+    
+    for (const gmapDiv of gmapsDivs) {            
+        var optsJson = gmapDiv.getAttribute("data-gmap-opts");
+        let opts = JSON.parse(optsJson);
 
-    Element.prototype.appendChild = function(element) {
-        const isGMapScript = element.tagName === 'SCRIPT' && /maps\.googleapis\.com/i.test(element.src);
-        const isGMapAccessScript = isGMapScript && urlCatchers.some(url => element.src.includes(url));
+        //console.log(opts);
+        
+        let actualGmap = new google.maps.Map(gmapDiv, {
+            center: new google.maps.LatLng(opts.center.lat, opts.center.lng),
+            zoom: opts.zoom,
+            disableDefaultUI: opts.disableDefaultUI
+        });
 
-        if (! isGMapAccessScript) {
-            return appendChild.call(this, element);
+        var infoWindow = new google.maps.InfoWindow({
+            content: "message",
+            maxWidth: 220
+        });
+
+        google.maps.event.addListener(actualGmap, "click", function () { infoWindow.close(); });
+        google.maps.event.addListener(actualGmap, "mouseout", function () { infoWindow.close(); });
+
+
+        let markerBounds = new google.maps.LatLngBounds();
+        let markers = [];
+
+        function makeMarker(options) {
+            let marker = new google.maps.Marker({map: options.map});
+            
+            marker.setOptions(options);
+            markerBounds.extend(options.position);
+            markers.push(marker);
+
+            if (options.infoWindowOpts.content) {
+                let infoWindowOpenOpts = {
+                    anchor: marker,
+                    map: options.map
+                };
+
+                google.maps.event.addListener(marker, 
+                    "click",
+                    function() {
+                        infoWindow.setOptions(options.infoWindowOpts);
+                        infoWindow.open(infoWindowOpenOpts);
+                    });
+
+                google.maps.event.addListener(marker, 
+                    "mouseover",
+                    function() {
+                        infoWindow.setOptions(options.infoWindowOpts);
+                        infoWindow.open(infoWindowOpenOpts);
+                    });
+            }
         }
-        return element;
-    };
-})();
+        
+        for(const markerOpt of opts.markers) {
+            const markerCenter = new google.maps.LatLng(markerOpt.lat, markerOpt.lng);
+            makeMarker({
+                position: markerCenter,
+                map: actualGmap,
+                animation: google.maps.Animation.DROP,
+                infoWindowOpts: {
+                    //position: markerCenter,
+                    content: markerOpt.infoWindowContent
+                }
+            });
+        }
+    }
+}
 
 
 $(document).ready(function() {
-    async function initMaps() {
-        const { Map } = await google.maps.importLibrary("maps");
-
-        const gmapsDivs = document.getElementsByClassName("gmap");
-        
-        for (const gmapDiv of gmapsDivs) {            
-            var optsJson = gmapDiv.getAttribute("data-gmap-opts");
-            let opts = JSON.parse(optsJson);
-
-            //console.log(opts);
-            
-            let actualGmap = new google.maps.Map(gmapDiv, {
-                center: new google.maps.LatLng(opts.center.lat, opts.center.lng),
-                zoom: opts.zoom,
-                disableDefaultUI: opts.disableDefaultUI
-            });
-
-            var infoWindow = new google.maps.InfoWindow({
-                content: "message",
-                maxWidth: 220
-            });
-
-            google.maps.event.addListener(actualGmap, "click", function () { infoWindow.close(); });
-            google.maps.event.addListener(actualGmap, "mouseout", function () { infoWindow.close(); });
-
-
-            let markerBounds = new google.maps.LatLngBounds();
-            let markers = [];
-
-            function makeMarker(options) {
-                let marker = new google.maps.Marker({map: options.map});
-                
-                marker.setOptions(options);
-                markerBounds.extend(options.position);
-                markers.push(marker);
-
-                if (options.infoWindowOpts.content) {
-                    let infoWindowOpenOpts = {
-                        anchor: marker,
-                        map: options.map
-                    };
-
-                    google.maps.event.addListener(marker, 
-                        "click",
-                        function() {
-                            infoWindow.setOptions(options.infoWindowOpts);
-                            infoWindow.open(infoWindowOpenOpts);
-                        });
-
-                    google.maps.event.addListener(marker, 
-                        "mouseover",
-                        function() {
-                            infoWindow.setOptions(options.infoWindowOpts);
-                            infoWindow.open(infoWindowOpenOpts);
-                        });
-                }
-            }
-            
-            for(const markerOpt of opts.markers) {
-                const markerCenter = new google.maps.LatLng(markerOpt.lat, markerOpt.lng);
-                makeMarker({
-                    position: markerCenter,
-                    map: actualGmap,
-                    animation: google.maps.Animation.DROP,
-                    infoWindowOpts: {
-                        //position: markerCenter,
-                        content: markerOpt.infoWindowContent
-                    }
-                });
-            }
-        }
-    }
-
-    initMaps();
+    gmapLoadAndFix("initMaps", "fr");
 });
 
